0222aea3ae83e5aa99af1bbe16b7005d8966c073
rahul kumar <rkrahul8181@gmail.com>
2023-04-09 09:54:51 +0530

    Created user and post models

---
+++ backend/src/models/user.model.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 1,
+    maxlength: 50
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/
+  },
+  bio: {
+    type: String,
+    maxlength: 200
+  }
+}, {
+  timestamps: true
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
